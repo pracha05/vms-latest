448e28d29aad2cdb1ccbbfbc5e9365d2e880d092
sumanta kanungo <sumanta.kanungo@mobicashindia.com>
2019-11-08 18:10:51 +0530

    Booking API,Add Load, allot booking API WIP, MAP activity updated with search action

--- app/build/generated/source/buildConfig/debug/com/vms/customer/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/vms/customer/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.1";
   // Fields from build type: debug
-  public static final String GIT_COMMIT_COUNT = "9";
-  public static final String GIT_HASH = "d424a4b";
+  public static final String GIT_COMMIT_COUNT = "10";
+  public static final String GIT_HASH = "9d87b90";
 }
